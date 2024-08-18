d04d800a3cd3624af7d9ecff32d42d106f1880dd
Andreas Ink <andreasink17@gmail.com>
2024-08-18 14:23:22 -0400

    add admin auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { jwtDecode } from 'jwt-decode'; 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -21,7 +21,7 @@ export class LoginComponent {
   
   onRegister() {
     const localUser = localStorage.getItem('users')
-
+    
     if(localUser != null) {
       const users = JSON.parse(localUser);
       users.push(this.signUpObj)
@@ -34,17 +34,39 @@ export class LoginComponent {
     alert('You successfully created your account!')
   }
 
+  getDecodedToken(token: string): any {
+    try {
+      return jwtDecode(token);
+    } catch (Error) {
+      return null;
+    }
+  }
+  // Gets token from local storage, I'll generate one for the demo and seed it into localStorage
+  isAdmin(): boolean {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkFkbWluIFVzZXIiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MjM5ODk4NzUsImV4cCI6MTcyMzk5MzQ3NX0.dBHr9amVPE9SDexosQxnllV5Ux_WKNkl9kDEYZ48XaM"// localStorage.getItem('authToken');
+    if (token) {
+      const decodedToken = this.getDecodedToken(token);
+      // Ensures the token exists and has an admin role
+      return decodedToken && decodedToken.role === 'admin';
+    }
+    return false;
+  }
+
   onLogin() {
     const localUsers = localStorage.getItem('users')
 
     if (localUsers != null) {
       const users = JSON.parse(localUsers)
-
+      
       const isUserPresent = users.find( (user:SignUpModel) => user.email == this.loginObj.email && user.password == this.loginObj.password)
       if (isUserPresent != undefined) {
         alert("User Found!");
         localStorage.setItem('loggedUser', JSON.stringify(isUserPresent));
-        this.router.navigateByUrl('/dashboard');
+        if(this.isAdmin()) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          alert("You are not an admin!");
+        }
       } else {
         alert('No User Found.')
       }
