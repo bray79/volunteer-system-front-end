dea060e4ef6dbc987252bbb7ef5aded89cac79f7
Micah van Meurs <n01454650@unf.edu>
2024-08-20 17:53:53 -0400

    Got some stuff working

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginModel } from '../login/login.component';
 import { Router } from '@angular/router';
+import { ManageOpportunitiesService } from '../manage-opportunities/manage-opportunities.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ManageOpportunities } from '../manage-opportunities/manage-opportunities';
+import { count, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-dash',
@@ -8,18 +13,49 @@ import { Router } from '@angular/router';
   styleUrl: './dash.component.css'
 })
 export class DashComponent {
+  
+  private apiUrlV = 'http://localhost:3000/volunteers';
+  private apiUrlO = 'http://localhost:3000/opportunities';
 
   loggedUser: any;
-
-  // Make this section after volunteers get section is added?
-  // let volunteers:
-
-  constructor(private router: Router) {
+  volunteers: Observable<ManageOpportunities[]>;
+  volunteersLength: number = 0;
+  opportunities: Observable<ManageOpportunities[]>;
+  opportunitiesLength: number = 0;
+  
+  constructor(private router: Router, private httpClient: HttpClient) {
     const localUser = localStorage.getItem('loggedUser');
 
     if (localUser != null) {
       this.loggedUser = JSON.parse(localUser);
     }
+
+    this.volunteers = this.getAllV()
+    this.opportunities = this.getAllO()
+  }
+
+  ngOnInit(): void {
+    this.volunteers.forEach(V => {
+      console.log(V)
+    })
+    this.volunteers.subscribe((x) => {
+      x.forEach(y => this.volunteersLength++)
+    })
+
+    this.opportunities.forEach(O => {
+      console.log(O)
+    })
+    this.volunteers.subscribe((x) => {
+      x.forEach(y => this.opportunitiesLength++)
+    })
+  }
+
+  getAllV(): Observable<ManageOpportunities[]> {
+    return this.httpClient.get<ManageOpportunities[]>(this.apiUrlV);
+  }
+
+  getAllO(): Observable<ManageOpportunities[]> {
+    return this.httpClient.get<ManageOpportunities[]>(this.apiUrlO);
   }
 
   onLogout() {
