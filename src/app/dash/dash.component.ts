78dd3d216c1a59c511dbeb9541113a867524133a
Micah van Meurs <n01454650@unf.edu>
2024-08-20 21:55:06 -0400

    Top 3 cards of admin dashboard are working. Now working on bottom 2 cards.

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { LoginModel } from '../login/login.component';
 import { Router } from '@angular/router';
-import { ManageOpportunitiesService } from '../manage-opportunities/manage-opportunities.service';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ManageOpportunities } from '../manage-opportunities/manage-opportunities';
+import { ManageVolunteers } from '../manage-volunteer/manage-volunteers';
 import { count, Observable } from 'rxjs';
 
 
@@ -18,8 +18,9 @@ export class DashComponent {
   private apiUrlO = 'http://localhost:3000/opportunities';
 
   loggedUser: any;
-  volunteers: Observable<ManageOpportunities[]>;
+  volunteers: Observable<ManageVolunteers[]>;
   volunteersLength: number = 0;
+  assignedVolunteers: number = 0;
   opportunities: Observable<ManageOpportunities[]>;
   opportunitiesLength: number = 0;
   
@@ -35,23 +36,27 @@ export class DashComponent {
   }
 
   ngOnInit(): void {
-    this.volunteers.forEach(V => {
-      console.log(V)
-    })
+    this.volunteers.forEach(V => { V.length; console.log("Volunteers:"); console.log(V) })
     this.volunteers.subscribe((x) => {
-      x.forEach(y => this.volunteersLength++)
+      x.forEach(y => {
+        this.volunteersLength++
+        // console.log(y.opportunities_ids.length)
+        if (y.opportunities_ids.length > 0) {
+          this.assignedVolunteers++
+        }
+      })
+      // console.log(x[3]["id"])
+      
     })
 
-    this.opportunities.forEach(O => {
-      console.log(O)
-    })
-    this.volunteers.subscribe((x) => {
-      x.forEach(y => this.opportunitiesLength++)
+    this.opportunities.forEach(O => { console.log("Opportunities:"); console.log(O) })
+    this.opportunities.subscribe((a) => {
+      a.forEach(b => this.opportunitiesLength++)
     })
   }
 
-  getAllV(): Observable<ManageOpportunities[]> {
-    return this.httpClient.get<ManageOpportunities[]>(this.apiUrlV);
+  getAllV(): Observable<ManageVolunteers[]> {
+    return this.httpClient.get<ManageVolunteers[]>(this.apiUrlV);
   }
 
   getAllO(): Observable<ManageOpportunities[]> {
